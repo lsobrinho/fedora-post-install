73ba0c790945d32397e325ab265fbdbaf07ce42f
lsobrinho <127059052+lsobrinho@users.noreply.github.com>
2025-03-09 16:42:35 -0300

    Create post_install.sh

---
+++ post_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Instala o RPM Fusion
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+# Instala o GNOME Tweaks
+sudo dnf install gnome-tweaks -y
+
+# Instala o GNOME Extensions
+sudo dnf install gnome-extensions-app -y
+
+# Corrige os problemas de codec
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+sudo dnf groupupdate sound-and-video -y
+sudo dnf install amrnb amrwb faad2 flac gpac-libs lame libde265 libfc14audiodecoder mencoder x264 x265 ffmpegthumbnailer -y
+
+# Instalar as fontes da Microsoft
+sudo dnf install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm -y
